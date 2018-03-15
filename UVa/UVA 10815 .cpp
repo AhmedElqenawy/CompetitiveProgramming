7dc852cbe2d2cb8b61dc38cd2a2b1bddc2fa3c56
AhmedElqenawy <36866792+AhmedElqenawy@users.noreply.github.com>
2018-03-15 17:23:02 +0200

    Update UVA 10815 .cpp

--- UVa/UVA 10815 .cpp
+++ UVa/UVA 10815 .cpp
@@ -1,32 +1,36 @@
 #include <iostream>
+#include <algorithm>
 #include <string>
+#include <vector>
+#include <cmath>
 #include <set>
 #include <map>
-#include <vector>
-#include <algorithm>
 using namespace std;
-set<string>st;
-int main() 
+map<int, int>mp;
+set<string>::iterator it;
+set <string> st;
+int main()
 {
-    string x,a;
-    set<string>::iterator t;
-    while(getline(cin,x)){
-         for(int i=0;i<x.length();i++){
-           if(isalpha(x[i])){
-                x[i]=tolower(x[i]);
-                a+=x[i];
-            }
-           else if(a!=""){
-               st.insert(a);
-               a="";
-            }
-    }
-    if(a!=""){
-      st.insert(a);
-      a="";
-    }
-}
-for (auto it:st)
-    cout<<it<<endl;
+	string x;
+	string z = "";
+	while (getline(cin, x))
+	{
+		for (int i = 0; i < x.length(); i++)
+		{
+			if (isalpha(x[i]))
+			{
+				x[i] = tolower(x[i]);
+				z += x[i];
+			}
+			else if (z != ""){ st.insert(z); z = ""; }
 
-    }
+		}
+		if (z != ""){ st.insert(z); z = ""; }
+
+	}
+	for (auto it : st)
+		cout << it << endl;
+	
+	
+	system("pause");
+}
